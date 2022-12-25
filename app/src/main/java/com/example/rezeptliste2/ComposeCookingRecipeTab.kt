104fefe5b1d71e9fc5b9bd5b3b63f8e67dc387b0
Downvote Hunter <25457206+Downvote-Hunter@users.noreply.github.com>
2022-12-25 21:46:22 +0100

    remove imports

--- app/src/main/java/com/example/rezeptliste2/ComposeCookingRecipeTab.kt
+++ app/src/main/java/com/example/rezeptliste2/ComposeCookingRecipeTab.kt
@@ -4,8 +4,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,16 +21,13 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rezeptliste2.database.controller.RecipeController
 import com.example.rezeptliste2.database.dto.Recipe
-import com.example.rezeptliste2.ui.theme.Rezeptliste2Theme
 import java.io.ByteArrayOutputStream
 
 
